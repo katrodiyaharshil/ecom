4117a67004a3e8efa332aaba9a1c9ea200ce6d2d
harshil <harshilkatrodiya2001@gmail.com>
2023-06-13 11:58:45 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 
 import './App.css';
-import { Button } from 'react-bootstrap';
+import AddProduct from './AddProduct';
+import UpdateProduct from './UpdateProduct';
+import Login from './Login';
+import Register from './Register';
+import Protected from './Protected';
+import ProductList from './ProductList';
+import { Routes,Route } from 'react-router-dom';
+import SearchProduct from './SearchProduct';
+
 
 function App() {
   return (
     <div className="App">
-      <Button>Hello</Button>
-      <button>Hello World</button>
+      
+      <Routes>
+        <Route path='/add' element={<Protected Cmd={AddProduct} />} />
+        <Route path="/update/:id" element={<Protected Cmd={UpdateProduct}/>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/search" element={<SearchProduct />} />
+        <Route path='/' element={<Protected Cmd={ProductList} />} />
+      </Routes>
     </div>
   );
 }
